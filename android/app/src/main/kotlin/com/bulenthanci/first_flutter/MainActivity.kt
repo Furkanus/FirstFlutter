f317f248a83b73cbfb2b50b495c0f5ce2de05702
Furkan Hanci <furkanhanci265@gmail.com>
2021-05-15 18:03:02 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/bulenthanci/first_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bulenthanci.first_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+     home: Scaffold(
+       body: Center(
+         child: Row(
+           children: [
+             Container(
+               width: 42,
+               height: 42.0,
+               padding: EdgeInsets.all(10.0),
+               decoration: BoxDecoration(
+                 borderRadius: BorderRadius.circular(15),
+                 gradient: LinearGradient(
+                   begin: Alignment.topLeft,
+                   end: Alignment.bottomRight,
+                   colors: [
+                     Color(0xFF00AEFF),
+                     Color(0xFF0076FF),
+                   ]
+                 )
+               ),
+               child: Icon(Icons.computer , color: Colors.white,)
+
+             ),
+             SizedBox(width: 12,),
+             Container(
+               child: Text("Computers" , style: TextStyle(
+                 fontSize: 16,
+                 fontWeight: FontWeight.bold,
+                   color : Color(0xFF242629)
+               ),),
+             ),
+           ],
+         )
+       ),
+     ),
+    );
+  }
+}
